c54581a1b7ed746e5037d7f1d6a85863dbed70d6
Ayman Mohamed <darknaya.am@gmail.com>
2025-12-26 15:35:30 +0200

    add firebase configuration to our project

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,235 @@
+abstract class AppStrings {
+  static const String appName = 'سِلْعَه';
+  static const String welcomeToWasally = 'مرحباً بك في $appName';
+  static const String phoneNumber = 'رقم الهاتف';
+  static const String login = 'تسجيل الدخول';
+  static const String deleteAccount = 'حذف الحساب';
+  static const String areYouSureToDeleteThisAccount =
+      '''هل أنت متأكد من حذف هذا الحساب؟
+  عند حذف الحساب، سيتم حذف جميع المعلومات المرتبطة به نهائياً
+  ''';
+
+  static const String signInFirst = 'اذهب إلى صفحة تسجيل الدخول للبدء';
+  static const String continueAsGuest = 'متابعة ك زائر';
+  static const String skip = 'تخط';
+  static const String verifyYourIdentity = 'تحقق من هويتك';
+  static const String shopName = 'اسم المتجر :';
+  static const String shopStatus = 'حالة المحل :';
+  static const String productName = 'اسم المنتج :';
+  static const String productPrice = 'السعر :';
+  static const String visitZCompany = 'زيارة موقع الشركة';
+  static const String searchAProduct = 'ابحث عن منتج';
+  static const String orderDeletedSuccessfully = 'تم حذف الطلب بنجاح';
+  static const String noProductsFoundInCategory =
+      'لا يوجد منتجات حالية ف هذا القسم';
+  static const String noSearchedProductsFound = 'لا توجد منتجات مطابقة للبحث';
+  static const String quantity = 'الكمية :';
+  static const String orderDate = 'التاريخ :  ';
+  static const String connectionTimeout = 'انتهت مهلة الاتصال';
+  static const String descreption = 'الوصف :';
+  static const String serverSendTimeout = 'انتهت مهلة الإرسال إلى الخادم';
+  static const String receiveTimeout = 'انتهت مهلة الاستلام من الخادم';
+  static const String noInternet = 'لا يوجد اتصال بالإنترنت';
+  static const String kilogram = '/ كيلو';
+  static const String searchAProductIn = 'ابحث عن منتج في';
+  static const String sorrythereWasAnErrorPleaseTryAgain =
+      'عذرًا، حدث خطأ. يرجى المحاولة مرة أخرى.';
+  static const String thereWasAnErrorPleaseTryAgain =
+      'حدث خطأ. يرجى المحاولة مرة أخري';
+  static const String thereWasAnErrorWhenVerifyingPleaseTryAgain =
+      'حدث خطأ أثناء التحقق. يرجى المحاولة مرة أخرى';
+  static const String sessionExpiredTryToLoginAgain =
+      'انتهت صلاحية رمز التحقق، حاول تسجيل الدخول مرة أخرى';
+  static const String thisPhoneNumberIsBlockedTemporarily =
+      'تم حظر هذا الرقم مؤقتًا، حاول لاحقًا';
+  static const String verificationCodeIncorrect = 'رمز التحقق غير صحيح';
+
+  static const String weSentVerifyCode =
+      'لقد أرسلنا رمز التحقق إلى رقم الهاتف المحمول';
+  static const String messageDosentSent = 'لم تصلك رسالة ؟';
+  static const String resendCode = 'أعد إرسال الرمز';
+  static const String verify = 'تأكيد';
+  static const String verifyDone = 'تم التحقق من رقم هاتفك بنجاح';
+  static const String start = 'ابدأ';
+  static const String goodMorrning = 'صباح الخير';
+  static const String goodAfternoon = 'مساء الخير';
+  static const String startOrderToday = 'ابدأ بالتسوق الان';
+  static const String searchCategory = '  ابحث عن فئة';
+  static const String cart = 'سلة المشتريات';
+  static const String delieryPrice = 'سعر التوصيل';
+  static const String totalPrice = 'المبلغ الكلي';
+  static const String itemsPrice = 'سعر المنتجات';
+  static const String completeOrder = 'تأكيد الطلب';
+  static const String getLocation = 'تحديد الموقع';
+  static const String orders = 'الطلبات';
+  static const String addToCart = 'إضافة لسلة المشتريات';
+  static const String orderDetails = 'تفاصيل الطلب';
+  static const String orderInfo = 'الطلبات';
+  static const String searchProduct = 'ابحث عن منتج';
+  static const String searchProductsDescription =
+      'اكتشف أفضل خيارات التسوق في مدينة الصالحية الجديدة بسهوله.';
+  static const String noProducts = 'لا توجد مشتريات حتي الان';
+  static const String goToShopping = 'اذهب الى التسوق';
+  static const String shoppingNow = 'تسوق الان';
+  static const String categories = 'الأقسام';
+  static const String wasallyOffers = 'أخبار و عروض $appName';
+  static const String offers = 'عروض';
+  static const String userName = 'الإسم';
+  static const String tryAgain = 'اعادة المحاوله';
+  static const String noShopsInCatigory = 'لا يوجد اي محلات في هذة الفئه حاليا';
+  static const String pleaseEnterOTP = 'الرجاء إدخال كود التحقق';
+  static const String pleaseEnterPhoneNumber = 'الرجاء إدخال رقم الهاتف';
+  static const String phoneNumberNotValid = 'رقم الهاتف غير صحيح';
+  static const String otpNotValid = 'كود التحقق غير صحيح';
+  static const String pleaseEnterYourName = 'من فضلك أدخل اسمك';
+  static const String searchForShop = 'ابحث عن متجر';
+  static const String useCurrentLocation = 'استخدم الموقع الحالي';
+  static const String changeLocation = 'تغيير الموقع';
+  static const String textUs = 'راسلنا';
+  static const String updateToContinue =
+      'يرجى تحديث التطبيق للاستمرار في استخدام وصلّى';
+  static const String updateNow = 'حدّث الآن';
+  static const String whatsappSupport =
+      'الدعم الفني الخاص بشركة $appName متاح من الساعة 10 صباحاً حتى 10 مساءاً';
+  static const String callUs = 'اتصل بنا';
+  static const String callSupport =
+      'الدعم الفني الخاص بشركة $appName متاح من الساعة 10 صباحاً حتى 10 مساءاً';
+
+  static const String cannotSelectThisLocation = 'لا يمكنك اختيار هذة الموقع';
+
+  static const String wasallyName = 'شركة $appName';
+  static const String contactUsIntro = '''
+$appName هو عالم متكامل للتسوق الذكي.
+ليس مجرد تطبيق، بل منصة تجمع كل ما يمكن تخيّله من سلع — من المنتجات اليومية البسيطة إلى أكثر الأشياء تفرّدًا وتميّزًا.
+في $appName، نربطك مباشرةً بأصحاب المتاجر والمورّدين، لتجد ما تبحث عنه بسهولة وبأسلوب يعكس احتياجاتك.
+حرية في الاختيار، تنوّع في السلع، وتجربة تسوّق مختلفة كليًا.
+
+$appName... كل ما تحتاجه وأكثر، في مكان واحد.
+''';
+  static const String confirmOrder = 'تأكيد الطلب';
+  static const String confirmLocation = 'تأكيد الموقع';
+  static const String searchingYourLocation = 'جاري تحديد موقعك';
+  static const String teamWork = 'فريق العمل';
+  static const String post = 'منشور';
+  static const String follower = 'متابع';
+  static const String followUsOnSocialMedia =
+      'تابعنا عن طريق مواقع التواصل الإجتماعي';
+  static const String faceBook = 'فيسبوك';
+  static const String instagram = 'انستجرام';
+  static const String wassallyCoFounders = 'شركاء $appName';
+  static const String noOrders = 'لا توجد طلبات حتي الان';
+  static const String cancelOrder = 'إلغاء الطلب';
+  static const String cancelOrderMessage =
+      'يمكنك فقط إلغاء الطلب في حالة انه لم يتم العمل عليه من خلال المحل.';
+  static const String attention = 'تنبيه!';
+  static const String ok = 'حسناً';
+  static const String couldNotLaunch = 'Could not launch';
+  static const String no = 'لا';
+  static const String yes = 'نعم';
+  static const String thisProductIsUnavailable = 'هذا المنتج غير متوفر مؤقتاً';
+  static const String orderCreatedSuccessfully = 'تم تأكيدالطلب بنجاح';
+  static const String price = 'السعر  : ';
+  static const String oldPrice = 'السعر القديم : ';
+  static const String newPrice = 'السعر الجديد : ';
+  static const String wasallyService = 'خدمة $appName';
+  static const String address = 'العنوان';
+  static const String date = 'التاريخ';
+  static const String time = 'الوقت';
+  static const String status = 'الحالة';
+  static const String orderPrice = 'ثمن الطلبات';
+  static const String deliveryPrice = 'ثمن التوصيل';
+  static const String driverName = 'اسم الطيار';
+  static const String driverPhone = 'رقم الهاتف';
+  static const String shopPhone = 'رقم المحل';
+  static const String youCanCallShopFromHere = 'يمكنك التواصل مع المحل من هنا';
+  static const String discountPercentage = 'نسبة الخصم/العرض : ';
+  static const String noInternetConnecton = 'لا يوجد اتصال بالإنترنت';
+  static const String open = 'مفتوح';
+  static const String busy = 'مشغول';
+  static const String closed = 'مغلق';
+  static const String save = 'حفظ';
+  static const String cancel = 'الغاء';
+  static const String logout = 'تسجيل الخروج';
+  static const String sureToLogout = 'هل انت متأكد من تسجل الخروج؟';
+  static const String enterName = 'ادخل الاسم';
+  static const String editName = 'تعديل الإسم';
+  static const String tapAgainToExit = 'إضغط مره اخري للخروج';
+  static const String goToSocialMediaToShowTheOffer =
+      'الذهاب لموقع التواصل الإجتماعى لتفقد العرض';
+  static const String forretningspartnere = 'شريك  العمل';
+  static const String zSoftware = 'Z Software Company';
+  static const String zSoftwareDescription =
+      'شركة برمجيات متخصصة في تطوير تطبيقات الموبايل والمواقع الإلكترونية، تقدم حلولاً تقنية متكاملة تساعد الشركات على تحقيق نمو حقيقي في عالم رقمي سريع التطور و تمتلك فريقاً من الخبراء في مجالات البرمجة، التصميم، وتجربة المستخدم، وتعمل بشغف على تحويل الأفكار إلى منتجات رقمية عالية الجودة. تولي اهتمامًا كبيرًا بتفاصيل الأداء، واجهات الاستخدام، وسهولة الوصول، لتضمن تجربة مثالية للمستخدم النهائي.';
+  static const String notificationNoInternetConnecton =
+      "No internet connection";
+  static const String notificationUnknownError = "Unknown notification error";
+  static const String tapAgainToExitApp = 'اضغط مره اخري للخروج من التطبيق';
+  static const String noRouteFound = 'No route found for';
+  static const String statusNotFound = 'status not found';
+  static const String resendDuring = 'يمكنك إعادة الإرسال خلال:';
+  static const String tokenExpired =
+      'تعذّر التحقق من حسابك. يرجى تسجيل الدخول مرة أخرى، أو التواصل مع الدعم إذا استمرّت المشكلة.';
+  static const String pleaseLoginAgain = 'الرجاء اعادة تسجيل الدخول مره اخري.';
+
+// server failures messages
+  static const String orderNotFound =
+      'لم يتم العثور على طلبك. يرجى المحاولة مرة أخرى.';
+  static const String orderTimeOut =
+      'انتهت مهلة الطلب. يرجى المحاولة مرة أخرى.';
+  static const String numberOfOrdersIsBig =
+      'عدد الطلبات كبير جدًا. يرجى التخفيف.';
+  static const String internalServerError =
+      'خطأ داخلي في الخادم. يرجى المحاولة مرة أخرى.';
+  static const String badGateway = 'بوابة سيئة. يرجى المحاولة مرة أخرى لاحقًا.';
+  static const String systemUnavailable =
+      'النظام غير متاح. يرجى المحاولة مرة أخرى لاحقًا.';
+  static const String unexpectedError =
+      'حدث خطأ غير متوقع. يرجى المحاولة مرة أخرى.';
+
+// location service messages
+  static const String locationServiceIsNotEnabled =
+      'Location service is not enabled.';
+  static const String locationPermissionIsDeniedForever =
+      'Location permission is denied forever.';
+  static const String locationPermissionIsDenied =
+      'Location permission is denied.';
+  static const String newSalihiyaCity = 'الصالحية الجديده';
+  static const String noAddressFound = 'No address found for the coordinates.';
+
+// firebase notification service messages
+  static const String awaitNotification =
+      'await notificationService.getDeviceToken()';
+
+// intl
+  static const String noInstanceLoaded =
+      'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.';
+  static const String noInstanceInWidgetTree =
+      'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?';
+
+// contact us phone numbers
+  static const String sel3aPhoneNumber = "201032713520";
+  static const String contactUsWhatsApp = "https://wa.me/$sel3aPhoneNumber";
+  static const String contactUsPhone = "tel://$sel3aPhoneNumber";
+
+  // onboarding
+  static const String onboardingTitleone = 'ابدأ رحلتك مع $appName';
+  static const String onboardingDescriptionone = '';
+  static const String onboardingTitletwo = 'ابحث وتسوق';
+  static const String onboardingDescriptiontwo =
+      'يمكنك الآن البحث عن أي منتج ترغب في شرائه، وسيعرض لك $appName أفضل الخيارات المتاحة لتجربة تسوق سهلة ومريحة';
+  static const String onboardingTitlethree = 'الدعم والمساعدة';
+  static const String onboardingDescriptionthree =
+      'هل تحتاج إلى مساعدة؟ فريق $appName جاهز لخدمتك والرد على استفساراتك في أوقات العمل المحددة';
+
+// order states
+  static const String orderStateDelivered = 'تم التوصيل';
+  static const String orderStateInProgress = 'قيد التنفيذ';
+  static const String orderStateCanceled = 'تم الالغاء';
+  static const String orderStatePending = 'قيد الانتظار';
+
+// search a product filter
+  static const String all = 'الكل';
+  static const String searchedOrders = 'العروض';
+  static const String mostPurchased = 'الاكثر شراءاً';
+  static const String lowestPrice = 'الاقل سعراً';
+}
